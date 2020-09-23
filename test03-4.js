44f958f295794bea75637d8fa699e9aff7c30138
fumi46 <71321992+fumi46@users.noreply.github.com>
2020-09-23 21:39:08 +0900

    j Query03課題4

---
+++ test03-4.js
@@ -0,0 +1,9 @@
+(function($, window) {
+    $(function() {
+      // ここに処理を書きます
+
+      $('#date_picker') .flatpickr({"locale": "ja"});
+      $('#date') .flatpickr({"locale": "ja"});
+      
+    });
+  })(jQuery, window);
